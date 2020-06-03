9238b2c1f93fed464a034d42f480b438ff1fb9e4
Manik <manikmhamud74@outlook.com>
2020-06-03 12:43:58 +0600

    single component  navbar

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,15 +1,5 @@
 <template>
- <ul class="list-group">
-	  <li class="list-group-item">
-	     Item Price
-		</li>
-		 <li class="list-group-item">
-	     Item Price
-		</li>
-		 <li class="list-group-item">
-	     Item Price
-		</li>
-	  </ul>
+
 </template>
 <script>
 
