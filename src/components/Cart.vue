230059c6d7aad6d74c3ac9dcec1cf72d133026a2
Manik <manikmhamud74@outlook.com>
2020-06-04 09:19:18 +0600

    single file components

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,15 @@
 <template>
-
+<ul class="list-group">
+	  <li class="list-group-item">
+	     Item Price
+		</li>
+		 <li class="list-group-item">
+	     Item Price
+		</li>
+		 <li class="list-group-item">
+	     Item Price
+		</li>
+	  </ul>
 </template>
 <script>
 
