359a1e29ed010086c5d3bd77b71d03d78bcf3959
Manik <manikmhamud74@outlook.com>
2020-06-05 08:26:06 +0600

    search items

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,8 +16,8 @@
       
       
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <form @submit.prevent="search" class="form-inline my-2 my-lg-0">
+      <input v-model="keyword" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
@@ -26,6 +26,16 @@
 <script>
 
 export default {
+  data(){
+    return{
+      keyword:''
+    }
+  },
+  methods:{
+search(){
+     this.$emit('search', this.keyword)
+   }
+  }
    
 }
 </script>
